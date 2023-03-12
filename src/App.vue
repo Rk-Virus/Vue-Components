21b2a06b63fb3c3817140b4f85dd5ed3c35d1938
Rk-virus <ravim70655@gmail.com>
2023-03-12 16:02:21 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TableComponent title="Table title"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TableComponent from './components/TableComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TableComponent
   }
 }
 </script>
